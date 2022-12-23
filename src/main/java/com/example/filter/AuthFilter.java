cd7225b29c8c039782ff2da922728253cd58d2b8
dadabaej <djam388@hotmail.com>
2022-12-24 00:41:10 +0500

    Attempt #1

--- src/main/java/com/example/filter/AuthFilter.java
+++ src/main/java/com/example/filter/AuthFilter.java
@@ -1,5 +1,51 @@
 package com.example.filter;
 
-public class AuthFilter {
+import com.example.Users;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(
+        urlPatterns = "/login"
+//        dispatcherTypes = {DispatcherType.REQUEST, DispatcherType.FORWARD}
+)
+public class AuthFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        boolean authorized = false;
+
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+
+        for (String user : Users.getInstance().getUsers()) {
+            if (user.equals(login) && password != null) {
+                authorized = true;
+            }
+        }
+
+        if (!authorized) {   //checking whether the session exists
+            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/login.jsp");
+        } else {
+
+            // pass the request along the filter chain
+            httpServletRequest.getSession().setAttribute("user", "user");
+            chain.doFilter(request, response);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+//        Filter.super.destroy();
+    }
     //write your code here!
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,35 @@
 package com.example.servlet;
 
-public class LoginServlet {
-    //write your code here!
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getSession().getServletContext().getRequestDispatcher("/login.jsp");
+        requestDispatcher.forward(request, response);
+//        request.getSession().getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request,HttpServletResponse response)
+            throws IOException
+    {
+        if (request.getSession().getAttribute("user").equals("user")) {
+            response.sendRedirect(request.getContextPath() + "/user/hello.jsp");
+        }
+        else {
+            response.sendRedirect(request.getContextPath() + "/login.jsp");
+        }
+
+
+
+    }
 }
